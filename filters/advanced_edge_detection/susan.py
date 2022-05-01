a2ac90e9043a29ff0de6e1d48720f953a6611986
eugepineiro <epineiro@itba.edu.ar>
2022-05-01 13:15:04 -0300

    chore adevanced methods structure

---
+++ filters/advanced_edge_detection/susan.py
@@ -0,0 +1,16 @@
+from PyQt5.QtGui import QPixmap 
+import qimage2ndarray
+import numpy as np
+import math
+
+
+class Susan():
+
+    def __init__(self, update_callback):
+        super().__init__(update_callback)
+
+
+    def apply(self, img):
+        width = img.width()
+        height = img.height()
+
