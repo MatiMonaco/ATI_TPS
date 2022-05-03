Fix gradient filter

filters/advanced_edge_detection/canny.py
@@ -1,3 +1,4 @@
+from dis import dis
 import numpy as np
 from filters.filter import Filter
 from PyQt5 import QtWidgets,QtCore
@@ -104,10 +105,12 @@ class Canny(Filter):
 
         # 3. Ángulo del gradiente para estimar la direccion ortogonal al borde
 
-        direction_angle = self.discretize_angle(np.arctan(dy_image/ dx_image)*180/np.pi) # Si dy = 0 arctan2 lo convierte en +-90º
+        discretized_angle,dirX,dirY = self.discretize_angle(np.arctan(dy_image/ dx_image)*180/np.pi) # Si dy = 0 arctan2 lo convierte en +-90º
+
+        print("Discretized angle = ",discretized_angle)
 
         # 4. Supresión de no máximos
-        self.no_max_supression(edge_magnitude_image, direction_angle)
+        self.no_max_supression(edge_magnitude_image, dirX,dirY)
 
         # 5. Umbralización con histéresis
         
@@ -120,31 +123,32 @@ class Canny(Filter):
             angle+=180
             print("new angle = ",angle)
         if (angle >= 0 and angle <= 22.5) or (angle >= 157.5 and angle <= 180): 
-            discretized_angle = 0
+            discretized_angle,dirX,dirY = 0,1,0
         
         elif angle > 22.5 and angle <= 67.5:
-            discretized_angle = 45 
+            discretized_angle,dirX,dirY = 45,1,-1
         
         elif angle > 67.5 and angle <= 112.5:
-            discretized_angle = 90
+            discretized_angle,dirX,dirY = 90,0,-1
         
         else: 
-            discretized_angle = 135 
+            discretized_angle,dirX,dirY = 135,-1,-1 
 
-        return discretized_angle
+        return discretized_angle,dirX,dirY
 
-    def no_max_supression(self, edge_magnitude_image, direction): 
-
-        for row in edge_magnitude_image.shape[0]: # TODO ojo con cual es el height y el width que la pifeo siempre
-            for col in edge_magnitude_image.shape[1]:
-
-                curr_pixel = edge_magnitude_image[row, col] # Para cada pixel con magnitud de borde no cero, inspeccionar los pixels adyacentes indicados en la dirección ortogonal al su borde.
-                if curr_pixel != 0:                     
-                    adjacent_pixels = self.get_adjacent_pixels(curr_pixel, direction)
-
-                    # Si la magnitud de cualquiera de los dos pixels adyacentes es mayor que la del pixel en cuestión, entonces borrarlo como borde.
+    def no_max_supression(self, edge_magnitude_image, dirX,dirY): 
         
-    def get_adjacent_pixels(self, curr_pixel, direction):
-        pass
+        for i in edge_magnitude_image.shape[0]: # TODO ojo con cual es el height y el width que la pifeo siempre <- esactamente
+            for j in edge_magnitude_image.shape[1]:
+                px1,px2 = edge_magnitude_image[i+dirX,j+dirY],edge_magnitude_image[i-dirX,j-dirY]
+                for channel in range(0,self.channels):   
+                    curr_pixel_channel = edge_magnitude_image[i, j,channel] # Para cada pixel con magnitud de borde no cero, inspeccionar los pixels adyacentes indicados en la dirección ortogonal al su borde.
+                    if curr_pixel_channel != 0:
+                        return 0                     
+                         # Agarro los 2 pixeles adyacentes en la direción del gradiente
+
+                        # Si la magnitud de cualquiera de los dos pixels adyacentes es mayor que la del pixel en cuestión, entonces borrarlo como borde.
+        
+            
 
 

filters/advanced_edge_detection/test.py
@@ -19,8 +19,16 @@ def discretize_angle(angle):
         return discretized_angle
 
 dx = 0
-dy = 1
+dy = -1
+
 angle = np.arctan2(dy,dx)* 180/np.pi
+x = np.cos(np.deg2rad(angle))
+y = np.sin(np.deg2rad(angle))
+print(f" x = {x} , y = {y}")
+x = np.sign(x) * np.ceil(np.abs(x))
+
+y = np.sign(y) * np.ceil(np.abs(y))
+print(f" x = {x} , y = {y}")
 
 print(f"angle = {angle }º")
 

filters/spatial_domain/border_detection/gradient_filter.py
@@ -2,8 +2,9 @@
 from filters.spatial_domain.spatial_domain_2 import SpatialDomainFilter
 import numpy as np
 import qimage2ndarray
-from PyQt5 import QtWidgets,QtCore
+from PyQt5 import QtWidgets
 import matplotlib.pyplot as plt
+import math
 class GradientFilter(SpatialDomainFilter):
 
     def __init__(self, update_callback):
@@ -55,6 +56,7 @@ class GradientFilter(SpatialDomainFilter):
         plt.show()
 
     def apply(self, img):
+     
         img_arr = qimage2ndarray.rgb_view(img).astype('int32')[:,:,0:self.channels]
 
         dx_mask = self.generate_dx_mask()
@@ -64,15 +66,15 @@ class GradientFilter(SpatialDomainFilter):
 
         # La mascara en x me detecta borders verticales
         self.dy_image = self.mask_filtering(
-            extended_img, dx_mask, padding_size)
+            extended_img, dx_mask, padding_size,norm=False)
         
         # La mascara en y me detecta borders horizontales
         self.dx_image = self.mask_filtering(
-            extended_img, dy_mask, padding_size)
+            extended_img, dy_mask, padding_size,norm=False)
        
-        border_magnitude = np.sqrt(self.dy_image**2 + self.dx_image**2)     
-        
-        return border_magnitude
+        edge_magnitude = np.sqrt(self.dy_image**2 + self.dx_image**2)     
+        #edge_magnitude = abs(self.dy_image) + abs(self.dx_image)     
+        return edge_magnitude
 
     def get_gradient(self):
         return self.dx_image,self.dy_image

filters/spatial_domain/spatial_domain_2.py
@@ -84,7 +84,7 @@ class SpatialDomainFilter(Filter):
         return np.array(new_img)
 
     def apply_mask(self, sub_img, mask=None):
-        # print(f"sub img before: {sub_img}")
+        #print(f"sub img before: {sub_img}")
         # print(f"--------------------------------------")
         # print(f"mask: {mask}")
         pixels_by_channel = []
@@ -92,7 +92,7 @@ class SpatialDomainFilter(Filter):
             pixels_by_channel.append(
                 np.sum(np.multiply(sub_img[:, :, channel], mask)))
 
-        # print(f"sub img after: {np.array(pixels_by_channel)}")
+        #print(f"pixel =  {np.array(pixels_by_channel)}")
         return np.array(pixels_by_channel)
 
     