6eb25a8ebc33b5d98edf18812f62bfc8103af66c
Matías <45739403+MatiMonaco@users.noreply.github.com>
2022-05-05 16:55:06 -0300

    Fix draw image in straight line

--- filters/feature_extraction/straight_line.py
+++ filters/feature_extraction/straight_line.py
@@ -1,4 +1,4 @@
-from PyQt5.QtGui import QPixmap 
+from PyQt5.QtGui import QPixmap
 import qimage2ndarray
 import numpy as np
 import math
@@ -7,54 +7,57 @@ from PIL import Image, ImageDraw
 
 # Override la funcion de la recta
 
+
 class HoughTransformStraightLine(HoughTransform):
 
     def __init__(self, update_callback):
         super().__init__(update_callback)
-        rho_range = math.sqrt(2)* max(200, 200)
+        rho_range = math.sqrt(2) * max(200, 200)
         self.rho_param = {
-            "param_name": "rho", 
+            "param_name": "rho",
             "min": -rho_range,
-            "max": rho_range, 
+            "max": rho_range,
             "parts": 10
-        } 
+        }
 
         self.theta_param = {
-            "param_name": "theta", 
+            "param_name": "theta",
             "min": -90,
-            "max": 90, 
+            "max": 90,
             "parts": 10
         }
         self.params = [self.theta_param, self.rho_param]
         self.params_len = len(self.params)
-        
 
-    def accumulate(self,x,y):
+    def accumulate(self, x, y):
 
-        for i in range(self.param_values_len[0]): # theta 
+        for i in range(self.param_values_len[0]):  # theta
             theta = self.param_values[0][i]
-            for j in range(self.param_values_len[1]): # rho 
+            for j in range(self.param_values_len[1]):  # rho
                 rho = self.param_values[1][j]
-                dist_to_line = self.calculate_distance_to_line(x,y,theta,rho)
+                dist_to_line = self.calculate_distance_to_line(
+                    x, y, theta, rho)
                 if dist_to_line < self.epsilon:
-                    self.accumulator[i,j] +=1
+                    self.accumulator[i, j] += 1
 
-
-    def calculate_distance_to_line(self,x,y,theta,rho):
+    def calculate_distance_to_line(self, x, y, theta, rho):
         # y*sen(theta) + x*cos(theta) = rho
         return abs(rho - x*math.cos(theta) - y*math.sin(theta))
-    
-    def straight_line(self, x, theta, rho): 
+
+    def straight_line(self, x, theta, rho):
         y = (rho - x*math.cos(theta)) / math.sen(theta)
         return y
 
-    def draw_figure(self, img_arr, lines): 
-        # line = [ theta, rho] 
-        print(img_arr)
-        img = Image.fromarray(img_arr.astype(np.uint8))
-        
-        draw = ImageDraw.Draw(img) 
-        for line in lines: 
+    def draw_figure(self, img_arr, lines):
+        # line = [ the
+        # ta, rho]
+        img_arr = img_arr.reshape((img_arr.shape[0], img_arr.shape[1]))
+        print(img_arr.shape)
+
+        img = Image.fromarray(img_arr.astype(np.uint8), mode='L')
+        print("aca")
+        draw = ImageDraw.Draw(img)
+        for line in lines:
             theta = line[0]
             rho = line[1]
 
@@ -71,6 +74,6 @@ class HoughTransformStraightLine(HoughTransform):
             #y1 = self.straight_line(x1, theta, rho)
             #y2 = self.straight_line(x2, theta, rho)
 
-            draw.line((x1,y1, x2,y2), fill=128)
-        
-        return  Image.getdata(img) 
+            draw.line((x1, y1, x2, y2), fill=128)
+
+        return np.asarray(img)
