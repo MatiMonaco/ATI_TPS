cfadebbe5af4097b01e7b13caec1b59f33de9064
Scott <scott.lin099@gmail.com>
2022-05-08 16:08:15 -0300

    draw_image: smaller image

--- libs/TP0/draw_image.py
+++ libs/TP0/draw_image.py
@@ -10,15 +10,18 @@ def draw_rect_iterative(it: int = 20):
     except FileExistsError:
         print("delete rects file in current folder")
         exit(-1)
-    top_left = 250
-    bottom_right = 350
+    size            = 200
+    rect_side_len   = 50
+    center          = int(size/2)
+    top_left        = center - int(rect_side_len/2)
+    bottom_right    = center + int(rect_side_len/2)
     draw_percentage = 0.8
-    max_draw = int(600 * draw_percentage)
-    step = (max_draw - bottom_right) / it
+    max_draw        = int(size * draw_percentage)
+    step            = (max_draw - bottom_right) / it
     print(f"step: {step}")
     for i in range(it):
         print(f"top_left: {top_left}, bottom_right: {bottom_right}")
-        im = Image.new('RGB', (600, 600), (0, 0, 0))
+        im = Image.new('RGB', (size, size), (0, 0, 0))
         draw = ImageDraw.Draw(im)
         draw.rectangle((top_left, top_left, bottom_right, bottom_right), fill=(255, 255, 255), outline=(255, 255, 255))
         im.save(f'rects/rect_{i}.pgm', quality=95) 
